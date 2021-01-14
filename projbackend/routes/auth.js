92e7c2eb04f645ae0222ee531d9d119722cd2ba2
faisal <faisal25march99@gmail.com>
2021-01-14 11:33:41 +0530

    Part 1 : Setting up validation routes

--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,10 +1,17 @@
 
 var express = require('express')
 var router = express.Router()
+const { check, validationResult } = require('express-validator');
+
 const { signOut, signUp} = require("../controllers/auth")
 
 
-router.post("/signup", signUp)
+router.post("/signup",[
+    // express.validator
+    check("name","must be at least 5 chars long").isLength({ min: 3 }),
+    check("email", "email is required").isEmail(),
+    check("password","password should be at least 3 char").isLength({ min : 3 })
+], signUp)
 router.get("/signout", signOut)
 
 
