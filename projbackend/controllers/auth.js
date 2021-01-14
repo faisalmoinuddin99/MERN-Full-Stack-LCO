a074814cf741e325f092c3eb1d92d37b5f6dd530
faisal <faisal25march99@gmail.com>
2021-01-14 11:01:25 +0530

    saving a user to database

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -1,8 +1,20 @@
+const User = require("../models/user")
 
 exports.signUp = (req, res) => {
-    console.log("REQ BODY", req.body);
-    res.json({
-        message : "Signup route works!"
+
+    // saving into the database
+    const user = new User(req.body)
+    user.save((error, user) => {
+        if(error){
+            return res.status(400).json({
+                error: "Not able to save user in DB"
+            })
+        }
+        res.json({
+            name : user.name,
+            email : user.email,
+            id : user._id
+        })
     })
 }
 exports.signOut = (req,res)=>{
